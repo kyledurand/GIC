7059ad2d2a746dec50bf62f175338b05c3b85c1b
Kyle Durand <kyle.durand@jadedpixel.com>
2016-05-07 11:54:07 -0400

    Content updated

--- safe-disposal-of-medical-waste.php
+++ safe-disposal-of-medical-waste.php
@@ -1,8 +1,11 @@
 <?
-$title = "Medical Waste Management Safety";
-$desc = "GICMD takes pride in bringing first-rate waste disposal management to medical and healthcare facilities.";
+  $title = "Safe Disposal Of Medical Waste - Sharps Chemical Biomedical";
+  $desc = "GICMD provides safe disposal of medical waste with the best sharps disposal containers, bio medical waste disposal bags and compliance training for staff";
+  $keywords = "safe disposal of medical waste, sharps disposal, sharps container, bio medical waste disposal bags, medical waste management";
 ?>
+
 <?php include 'header.php'; ?>
+
 <div class="shadow mainImage center mgnT14">
   <img src="img/safety.jpg" width="960" height="107" alt="Medical Waste Removal Safety">
 </div>
@@ -10,19 +13,47 @@ $desc = "GICMD takes pride in bringing first-rate waste disposal management to m
 <div class="clear"></div>
 <div class="contentBox mgnBI">
   <div class="contentLeft">
-    <h1>Your Safety Is of Utmost Importance To Us</h1>
+    <h1>Strict Compliance To Rules! You Can Count On Safe Disposal Of Medical Waste</h1>
     <div class="lineRuleSolid"></div>
     <div id="left">
       <p>
-        Medical waste can be infectious and potentially dangerous,  putting many lives at risk. When you employ GICMD&rsquo;s services, we will take full  responsibility of properly handling and disposing your medical wastes and other  hazardous wastes.
+        Safe disposal of medical waste is crucial because medical waste can be infectious and potentially dangerous to your staff and the public.  If medical waste management is not handled properly, it can put many lives at risk.
       </p>
+      <p>
+        When you employ GICMD&rsquo;s services, we will take full  responsibility of properly handling and disposing your medical wastes and other  hazardous wastes.
+      </p>
+      <h2 class="quote">
+        We Equip You With The Right Tools For Safe Disposal Of Medical Waste
+      </h2>
       <p>
         Along with our promise to deliver the most efficient,  cost-effective waste disposal solutions, we also guarantee the safety and  well-being of our customers, their employees, their own clients, and the  general public.
       </p>
       <p>
-        A vital aspect of proper waste disposal is using the right  tools and foolproof equipment. GICMD offers a wide assortment of waste disposal  products (i.e. medical waste bags, waste containers and bins, and gloves) to  help protect you from harm and injury. These products are made from high-grade  materials to certify durability and sturdiness
+        A vital aspect of proper medical waste disposal is using the right tools and proven, foolproof, state-of-the-art equipment.  GICMD can provide the best waste disposal solution for:
       </p>
-      <h3> Our Promise of Safety</h3>
+      <ul>
+        <li>Sharps container disposal</li>
+        <li>Sharps bin disposal</li>
+        <li>Sharps containers</li>
+        <li>Needle disposal</li>
+        <li>Syringe disposal</li>
+        <li>Other medical waste containers</li>
+        <li>biohazard bags</li>
+        <li>biohazardous waste</li>
+        <li>hospital waste management</li>
+        <li>Medical waste disposal containers</li>
+        <li>Medical gloves</li>
+        <li>Custom solutions for your needs</li>
+      </ul>
+      <p>
+        These products are made from high-grade materials to certify durability and sturdiness.
+      </p>
+      <h2>
+        GICMD Provides Staff Training In Safety Procedures And Compliance Regulations
+      </h2>
+      <h3>
+        We Custom Design A Plan For Safe Disposal Of Medical Waste To Protect Your Staff And The Public
+      </h3>
       <p>
         GICMD takes pride in bringing first-rate waste disposal  management to medical and healthcare facilities. Our standard procedures are  designed to reduce the risk of an accident and/or injury — that our customers  can freely move around their facility and work without worry.
         <br>
@@ -42,8 +73,12 @@ $desc = "GICMD takes pride in bringing first-rate waste disposal management to m
         </li>
       </ul>
       <p>
-        Experience  the GICMD advantage when you call us at
-        <strong><?= $to_phone; ?></strong> (toll free) today. Collaborate with our team of specialist to build the most  convenient package deal for your medical waste disposal.
+        Experience the GICMD advantage of safe medical waste disposal when you call us at
+        <strong><?= $to_phone; ?></strong>
+        (toll free) today.
+      </p>
+      <p>
+        Collaborate with our team of specialist to build the most  convenient package deal for your medical waste disposal.
       </p>
     </div>
     <br>
