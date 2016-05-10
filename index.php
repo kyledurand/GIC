b7dd5f50f8dec7638584aeb551376aefd3976798
Kyle Durand <kyle.durand@jadedpixel.com>
2016-05-10 01:41:30 -0400

    Improving squishiness

--- index.php
+++ index.php
@@ -1,48 +1,46 @@
 <!doctype html>
 <html>
-  <?php
+<?php
   ob_start();
   error_reporting(E_ERROR);
   $title = "Hazardous Waste Management & Medical Disposal Toronto & Mississauga";
   $desc = "GIC Medical Disposal is a pharmaceuticals and hazardous waste disposal & waste management company serving Toronto, Mississauga, Niagara Falls & Ottawa.";
   $keywords = "waste management Toronto, hazardous waste disposal Toronto, waste management Mississauga";
-  ?>
+?>
 
-  <?php include 'header.php'; ?>
+<?php include 'header.php'; ?>
 
-  <body>
-    <div id="slides">
-      <div class="slides_container">
-        <div class="idx1" onclick="MM_goToURL('parent', 'Services');
-        return document.MM_returnValue">
-        <div class="mainImageText floatRight">
+<body>
+  <div id="slides">
+    <div class="slides_container">
+      <div class="idx1" onclick="MM_goToURL('parent', 'Services');
+      return document.MM_returnValue">
+      <div class="mainImageText floatRight">
 
-          <div class="textBox1 floatLeft">
-            <h2 class="hero-title">
-              <span style="color:#006db7;">SAVING</span> YOU <span style="color:#006db7;">TIME</span> AND <span style="color:#006db7;">MONEY</span>
-            </h2>
-          </div>
-
-          <div class="textBox2 floatRight">
-            <h5>GIC’s expert advice, smart products and great
-              service can help you reduce costs and increase
-              efficiency.</h5>
-            </div>
-          </div>
-
-          <a href="Services">
-            <div class="btn">
-              <h6>CLICK HERE</h6>
-              <p>TO LEARN MORE</p>
-            </div>
-          </a>
+        <div class="textBox1 floatLeft">
+          <h2 class="hero-title">
+            <span style="color:#006db7;">SAVING</span> YOU <span style="color:#006db7;">TIME</span> AND <span style="color:#006db7;">MONEY</span>
+          </h2>
         </div>
 
-        <div class="idx2" onclick=
-        "MM_goToURL('parent', 'Eco-Friendly');
-        return document.MM_returnValue">
-        <div class="mainImageText floatRight">
+        <div class="textBox2 floatRight">
+          <h5>GIC’s expert advice, smart products and great
+            service can help you reduce costs and increase
+            efficiency.</h5>
+          </div>
+        </div>
 
+        <a href="Services">
+          <div class="btn">
+            <h6>CLICK HERE</h6>
+            <p>TO LEARN MORE</p>
+          </div>
+        </a>
+      </div>
+
+      <div class="idx2" onclick="MM_goToURL('parent', 'Eco-Friendly');
+      return document.MM_returnValue">
+        <div class="mainImageText floatRight">
           <div class="textBox1 floatLeft">
             <h2 class="hero-title">
               <span style="color:#006db7;">ECOLOGICALLY</span> SUSTAINABLE
@@ -60,14 +58,11 @@
             <p>TO LEARN MORE</p>
           </div>
         </a>
-
       </div>
 
-      <div class="idx3" onclick=
-      "MM_goToURL('parent', 'Start-Service');
+      <div class="idx3" onclick="MM_goToURL('parent', 'Start-Service');
       return document.MM_returnValue">
       <div class="mainImageText floatRight">
-
         <div class="textBox1 floatLeft">
           <h2 class="hero-title"><span style="color:#006db7;">MEDICAL</span> WASTE DISPOSAL <span style="color:#006db7;">SERVICE</span></h2>
         </div>
@@ -83,9 +78,7 @@
           <p>TO LEARN MORE</p>
         </div>
       </a>
-
     </div>
-
   </div>
 
   <div class="prev">
@@ -122,89 +115,112 @@
     <h1 class="seo-h2-meta-tag">Our Enviromental Services Are:</h1>
 
     <div class="bucket fl">
-      <p class="safe"><b>Safe &amp; Easy.</b> Proper handling and
-        disposal of biomedical waste can be a daunting task for
-        healthcare, veterinary and laboratory professionals. Medical
-        waste can be infectious and may harbour organisms with the
-        potentially harmful capacity to self-replicate...<span class=
-        "moreInfo"><a href="Safety.php">More Information
-        &gt;</a></span></p>
-      </div>
+      <p class="safe">
+        <b>Safe &amp; Easy.</b>
+        <span class="media__helpers--hide-before-medium">
+          Proper handling and disposal of biomedical waste can be a daunting task for
+          healthcare, veterinary and laboratory professionals. Medical
+          waste can be infectious and may harbour organisms with the
+          potentially harmful capacity to self-replicate...
+        </span>
+        <span class="moreInfo">
+          <a href="Safety.php">More Information &gt;</a>
+        </span>
+      </p>
+    </div>
 
-      <div class="bucket fl">
-        <p class="convenient"><strong>Convenient Solutions.</strong> At
-          GIC, we pride ourselves in providing flexible and
+    <div class="bucket fl">
+      <p class="convenient">
+        <b>Convenient Solutions.</b>
+        <span class="media__helpers--hide-before-medium">
+          At GIC, we pride ourselves in providing flexible and
           customer-focused services that are tailored to meet your unique
           needs - we offer you the freedom and flexibility of choice.
           Whether you require a daily, weekly, bi-weekly,
-          monthly...<span class="moreInfo"><a href="About-Us.php">More
-          Information &gt;</a></span></p>
-        </div>
+          monthly...
+        </span>
+        <span class="moreInfo">
+          <a href="About-Us.php">More Information &gt;</a>
+        </span>
+      </p>
+    </div>
 
-        <div class="bucket fl clear">
-          <p class="costeffective"><strong>Cost Effective.</strong> The
-            implementation of GIC’s solutions can immediately realize user
-            savings through enhanced efficiency. Our team is always
-            available to offer expert advice, smart products and the
-            quality service you expect and deserve...<span class=
-            "moreInfo"><a href="About-Us.php">More Information
-            &gt;</a></span></p>
-          </div>
+    <div class="bucket fl clear">
+      <p class="costeffective">
+        <b>Cost Effective.</b>
+        <span class="media__helpers--hide-before-medium">
+          The implementation of GIC’s solutions can immediately realize user
+          savings through enhanced efficiency. Our team is always
+          available to offer expert advice, smart products and the
+          quality service you expect and deserve...
+        </span>
+        <span class="moreInfo">
+          <a href="About-Us.php">More Information &gt;</a>
+        </span>
+      </p>
+    </div>
 
-          <div class="bucket fl">
-            <p class="ecofriendly"><b>Environmentally Friendly.</b> GIC
-              goes the extra mile by implementing extra-stringent,
-              environmentally-focused quality control procedures that further
-              regulate the handling, transportation, storage, treatment and
-              disposal of medical waste...<span class="moreInfo"><a href=
-              "Eco-Friendly.php">More Information &gt;</a></span></p>
-            </div>
+    <div class="bucket fl">
+      <p class="ecofriendly">
+        <b>Environmentally Friendly.</b>
+        <span class="media__helpers--hide-before-medium">
+          GIC goes the extra mile by implementing extra-stringent,
+          environmentally-focused quality control procedures that further
+          regulate the handling, transportation, storage, treatment and
+          disposal of medical waste...
+        </span>
+        <span class="moreInfo">
+          <a href="Eco-Friendly.php">More Information &gt;</a>
+        </span>
+      </p>
+    </div>
 
-            <div class="clear"></div>
+    <div class="clear"></div>
 
-            <h2 class="seo-h2-meta-tag">Hazardous & Biomedical Waste Management In Compliance With:</h2>
+    <h2 class="seo-h2-meta-tag">Hazardous & Biomedical Waste Management In Compliance With:</h2>
 
-            <div class="lineRuleSolid"></div>
+    <div class="lineRuleSolid"></div>
 
-            <div class="fl ac small compliance-image">
-              <p>
-                <a href="http://www.ec.gc.ca/?lang=En" target="_blank">
-                  <img alt="waste management toronto &amp; mississauga" border="0" src="img/badges2/environment-canada.jpg">
-                </a>
-              </p>
-              <p>Environment Canada</p>
-              </div>
+    <div class="compliances">
+      <div class="compliance-image">
+        <p>
+          <a href="http://www.ec.gc.ca/?lang=En" target="_blank">
+            <img alt="waste management toronto &amp; mississauga" border="0" src="img/badges2/environment-canada.jpg">
+          </a>
+        </p>
+        <p>Environment Canada</p>
+      </div>
 
-              <div class="fl mgnLB ac small compliance-image">
-                <p>
-                  <a href="http://www.epa.gov/" target="_blank">
-                    <img alt="environmental protection agency" border="0" src="img/badges2/epa-logo.gif">
-                  </a>
-                </p>
-                <p>US Environment Agency</p>
-                </div>
+      <div class="compliance-image">
+        <p>
+          <a href="http://www.epa.gov/" target="_blank">
+            <img alt="environmental protection agency" border="0" src="img/badges2/epa-logo.gif">
+          </a>
+        </p>
+        <p>US Environment Agency</p>
+      </div>
 
-                <div class="fl mgnLB ac small compliance-image">
-                  <p>
-                    <a href="http://ec.europa.eu/environment/index_en.htm" target="_blank">
-                      <img alt="basel convention" border="0" src="img/badges2/bc-unep.jpg">
-                    </a>
-                  </p>
-                    <p>European Environment Protection</p>
-                  </div>
+      <div class="compliance-image">
+        <p>
+          <a href="http://ec.europa.eu/environment/index_en.htm" target="_blank">
+            <img alt="basel convention" border="0" src="img/badges2/bc-unep.jpg">
+          </a>
+        </p>
+        <p>European Environment Protection</p>
+      </div>
 
-                  <div class="fl mgnLB ac small compliance-image">
-                    <p>
-                      <a href="http://ec.europa.eu/environment/index_en.htm" target="_blank">
-                        <img alt="Basel Convention" src="img/badges2/basel.gif"></a>
-                      </p>
-                      <p>European Basel Convention</p>
-                    </div>
-
-                    <div class="clear mgnB30"></div>
-                  </div>
-                  <?php include 'rightInd.php'; ?>
-                  <?php include 'footer.php'; ?>
-                </div>
-              </body>
-              </html>
+      <div class="compliance-image">
+        <p>
+          <a href="http://ec.europa.eu/environment/index_en.htm" target="_blank">
+            <img alt="Basel Convention" src="img/badges2/basel.gif">
+          </a>
+        </p>
+        <p>European Basel Convention</p>
+      </div>
+    </div>
+  </div>
+  <?php include 'rightInd.php'; ?>
+  <?php include 'footer.php'; ?>
+</div>
+</body>
+</html>
