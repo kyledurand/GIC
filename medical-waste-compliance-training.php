1224624a1d575c31aa01a10fba64bd6874e233d0
Kyle Durand <kyle.durand@jadedpixel.com>
2016-05-29 16:40:39 -0400

    Updating compliance page

---
+++ medical-waste-compliance-training.php
@@ -0,0 +1,68 @@
+<?
+  $title    = "Medical Waste Compliance Training Program - GIC Medical";
+  $desc     = "If your staff doesn't have the latest medical waste compliance training, even the best waste disposal equipment can endanger your employees or patients";
+  $keywords = "medical waste compliance, waste management Compliance, medical waste Compliance, waste Compliance, hazardous material compliance";
+?>
+
+<?php include 'header.php'; ?>
+
+<div class="shadow mainImage center mgnT14">
+  <img src="img/contact.jpg" width="960" height="107" alt="About Us">
+</div>
+<br>
+<div class="clear"></div>
+<div class="contentBox mgnBI">
+  <div class="contentLeft">
+    <h1>Staff Safety Awareness And Medical Waste Compliance Training Program</h1>
+    <div class="lineRuleSolid"></div>
+    <div id="left">
+
+      <p>  
+        Up to date, clearly spelled out, easy to follow medical waste compliance training.
+      </p>
+
+      <p>
+        Count on GICMD to help you and your staff learn the
+        correct  methodologies to take care of your facility&rsquo;s medical
+        wastes. We will work  together with you to develop complete safety
+        awareness among your medical staff  and employees.
+      </p>
+
+      <h3>Learn from our Medical Waste Compliance Professionals</h3>
+      
+      <img src="img/Compliance-Training.jpg" width="258" height="172" alt="waste disposal compliance training canada" class="stock">
+      
+      <p>
+        Our experts always deliver a comprehensive medical waste compliance
+        workshop through our Compliance Training Program by teaching:
+        <ul>
+          <li>Safe methods of handling biomedical and  pharmaceutical wastes</li>
+          <li>Strict guidelines to follow in discarding used  needles and other sharps wastes</li>
+          <li>Proper segregation of biohazard wastes using  yellow and red bags</li>
+          <li>Appropriate labelling and handling of medical  wastes bags</li>
+          <li>Correct use of medical containers and bins</li>
+          <li>Regulations regarding disposal of chemical  wastes</li>
+        </ul>
+      </p>
+      <p>
+        Our Compliance Training Program is structured after the  strict guidelines established by Environment Canada. We will also instruct you  about the policies and regulated standards outlined in your municipal and  provincial legislations. We will also assess and evaluate your current medical  waste management if it is up to legal standards — one that guarantees safety of  everyone and the environment.
+      </p>      
+      <h3>Collaborate with our GICMD Trainers</h3>
+      <p>        
+        Sign up for the GICMD&rsquo;s Compliance Training Program to be  part of the solution! Ensure that your medical wastes do not put the health of  many at risk, but at the same time develop your facility&rsquo;s workflow with ease  and convenience. Take advantage of GICMD&rsquo;s cost-effective and secured disposal  services.<br>
+        Put your confidence in GICMD to facilitate a safe and  compliant waste disposal system in your facility. Consult with our team of  specialists today by calling <strong><?= $to_phone; ?></strong> (toll free).<br>
+      </p>
+      <p>
+        <strong>
+          Put your confidence in GICMD to facilitate a safe and compliant waste
+          disposal system in your facility.  Consult with our team of medical
+          waste compliance specialists today by calling <?= $to_phone; ?>
+        </strong>
+      </p>
+    </div>
+    <br>
+    <div class="clear"></div>
+  </div>
+
+  <?php include 'right.php'; ?>
+  <?php include 'footer.php'; ?>
