d0f75fd178663aa71c46d62212bfab1ed9a7a5bc
Kyle Durand <kyle.durand@shopify.com>
2016-08-08 10:03:06 -0400

    Updating sitemap

--- medical-waste-1.php
+++ medical-waste-1.php
@@ -1,7 +1,7 @@
 <?
-  $title = "Welcome to the GIC Advantage";
-  $desc = "GICMD is committed to providing industry-leading solutions when it comes to sharps disposal management. Safety is our main priority. Call us.";
-  $disallowBots = true;
+$title = "Medical Waste Disposal";
+$desc = "GICMD is committed to providing industry-leading solutions when it comes to sharps disposal management. Safety is our main priority. Call us.";
+$disallowBots = true;
 ?>
 
 <?php include 'components/header.php'; ?>
@@ -14,190 +14,105 @@
 
 <div class="contentBox mgnBI">
   <div class="contentLeft">
-  <h1>Medical Waste Disposal</h1>
+    <h1>Medical Waste Disposal Services Offered</h1>
     <div class="lineRuleSolid"></div>
-    <h3>
-      NOT Receiving the high level of Service YOU DESERVE from Your Medical Disposal
-      Company? <br>Are you getting billed monthly and only receiving service a few times a
-      year?<br> Are the services not being performed on the date they've been scheduled
-      on?
-    </h3>
+
+    <ul>
+      <li>Biomedical Waste Disposal</li>
+      <li>Sharps Waste Disposal</li>
+      <li>Chemical Waste Disposal</li>
+      <li>Pharmaceuticals Waste Disposal</li>
+      <li>Sustainability Services</li>
+      <li>Compliance Training</li>
+      <li>Hazardous/Industrial Waste Disposal</li>
+      <li>Vacuum Truck Service</li>
+      <li>Hazardous Material Transportation Service</li>
+      <li>Environmental Remediation / Spill Clean Up</li>
+      <p>
+        We also offer a wide assortment of eco-friendly waste disposal products/.
+        Find out which one is the perfect fit for your business and facility.
+      </p>
+      <li>Sharps waste container, disposable or reusable</li>
+      <li>Biomedical bins and containers</li>
+      <li>Pharmaceutical pails, totes, and drums</li>
+      <li>Chemical pails, totes, and drums</li>
+    </ul>
+
+    <h2>NOT Receiving the high level of Service YOU DESERVE from Your Medical Waste Disposal Company?</h2>
+
+    <ul>
+      <li>Are you getting billed monthly and only receiving service a few times a year?</li>
+      <li>Are the services not being performed on the date they've been scheduled on?</li>
+    </ul>
+
     <div id="left">
-      <h3>Welcome to the GIC Advantage</h3>
+      <h2>Welcome to the GIC Advantage</h2>
 
-      <h4>
-        We Provide a customer service focused Medical Waste Disposal Solution That Meets
-        &amp; Exceeds Compliance to ensure Safety of your Staff &amp; the Environment...
-      </h4>
+      <b>We Provide a customer service focused Medical Waste Disposal Solution That Meets &amp;
+        Exceeds Compliance to ensure Safety of your Staff & the Environment...
+        Rest Assured that you will receive our renowned service at the BEST VALUE.
+      </b>
 
-      <h3>Rest Assured that you will receive our renowned service at the BEST VALUE.</h3>
+      <h3>GET A MINIMUM OF 10% OFF YOUR CURRENT SERVICE PROVIDER GUARANTEED! <br>
+        It's time for a change. It's time you experience the best medical waste disposal service and lowest rates!
+      </h3>
 
-      <h4>GET A MINIMUM OF 10% OFF YOUR CURRENT SERVICE PROVIDER GUARANTEED!</h4>
+      <h2>Here's Just A Few Things You'll Achieve When You Begin Receiving The GIC Advantage Today... </h2>
 
-      <h3>It's time for a change. It's time you experience the best service and lowest
-        rates!</h3>
+      <p>Safety</p>
+      <p>We guarantee the safety and well-being of our customers, their employees, clients, and the general public.</p>
 
-        <div class="ac">
-          <a href="http://app.funneltrax.com/traffic/traffic-count/6037" class="bigGreenButton bigGreenButton--inline titleCase spaceAround">Get a free quote</a>
-        </div>
+      <ul>
+        <li>Reduce the risk of an accident and/or injury with the latest industry best- practices so you can freely move around your facility and work without worry. </li>
+        <li>Protect yourself and your staff from harm and injury with training on the proper management of medical wastes, appropriate use of medical waste bags and other equipment. </li>
+        <li>Reduce your legal liabilities by instituting custom procedures for needle stick and other injuries.< </li>
+        <li>Streamline your disposal process with a comprehensive structure for collection, segregation, handling, and disposal of medical wastes and hazardous chemicals. </li>
+        <li>Get access to industry leading waste disposal tools and highest safety rated equipment products that are required for your business. </li>
+      </ul>
 
-        <p>
-          Here's Just A Few Things You'll Achieve When You Begin Receiving The GIC
-          Advantage Today...
-        </p>
+      <p>Environmentally Friendly</p>
 
-        <h3>Safety</h3>
+      <p><strong>Get peace of mind that your waste has been disposed of appropriately, without harming the environement or anyone in it. </strong></p>
 
-        <h4>
-          We guarantee the safety and well-being of our customers, their employees,
-          clients, and the general public.
-        </h4>
+    <ul>
+      <li>GICMD experts scan all waste bags and containers upon pick-up. This helps track your waste products during transportation until it reaches its disposal destination. </li>
+      <li>GICMD guarantees your medical waste is safely disposed according to the rules and regulations set by the Ministry of Environment, Health Canada & Environment Canada. </li>
+    </ul>
 
-        <ul>
-          <li>
-            Reduce the risk of an accident and/or injury with the latest industry best-
-            practices so you can freely move around your facility and work without worry.
-          </li>
-          <li>
-            Protect yourself and your staff from harm and injury with training on the proper
-            management of medical wastes, appropriate use of medical waste bags and other
-            equipment.
-          </li>
-          <li>
-            Reduce your legal liabilities by instituting custom procedures for needle stick
-            and other injuries.<
-          </li>
-          <li>
-            Streamline your disposal process with a comprehensive structure for collection,
-            segregation, handling, and disposal of medical wastes and hazardous chemicals.
-          </li>
-          <li>
-            Get access to industry leading waste disposal tools and highest safety rated
-            equipment products that are required for your business.
-          </li>
-        </ul>
+    <img src="img/mid-banner-1.jpg" width="605" height="236" alt="Guaranteed Compliance">
 
-        <h3>Environmentally Friendly</h3>
+    <p><strong>Guaranteed Compliance</strong></p>
+    <p>We are committed to fully eliminating any lingering hazmat imprint, or the environmental threat of biomedical waste – Let GIC Medial Disposal shield your facility from any non-compliance penalties.</p>
 
-        <h4>
-          Get peace of mind that your waste has been disposed of appropriately, without
-          harming the environement or anyone in it.
-        </h4>
+    <p>It's no secret, extreme caution is required when handling medical waste. The provincial and federal governments have stringent laws and regulations in place regarding the management of medical waste. It may be confusing to sort through all the information available and stay on top of regulations. Improper waste management that is not in compliant with government regulations, can lead to major complications. </p>
+    <p>Count on GICMD to help you and your staff learn the correct methodologies to take care of your facility's medical waste. We will work together with you to develop complete safety awareness among your medical staff and employees. </p>
+    <p>We advocate and exercise rigid and compliant procedures, which altogether reduce the risk of accident and/or injury while giving you the freedom to effortlessly focus your attention on managing other areas of your business. </p>
 
-        <ul>
-          <li>
-            GICMD experts scan all waste bags and containers upon pick-up. This helps track
-            your waste products during transportation until it reaches its disposal
-            destination.
-          </li>
-          <li>
-            GICMD guarantees your medical waste is safely disposed according to the rules
-            and regulations set by the Ministry of Environment, Health Canada &amp; Environment
-            Canada.
-          </li>
-        </ul>
+    <p><strong>Premium Level Customer Service</strong></p>
 
-        <img src="img/mid-banner-1.jpg" width="605" height="236" alt="Guaranteed Compliance">
+    <p>A compliant, efficient and unparalleled customer-focused solution will be designed to meet YOUR specific requirements. </p>
 
-        <h3>Guaranteed Compliance</h3>
+    <ul>
+      <li>Always available during business hours.</li>
+      <li>You will ALWAYS be able to speak to someone that can address your concerns.</li>
+      <li>Flexible – We will customize our service for YOUR specific needs – If you need us to pick up more often, or less often… We will adjust our service to fits your facilities' requirements and operating hours.</li>
+      <li>Industry leading products that meet the highest standards</li>
+      <li>No long-term contracts</li>
+      <li>No monthly/hidden fees</li>
+      <li>Courteous, fully insured staff</li>
+    </ul>
 
-        <h4>
-          We are committed to fully eliminating any lingering hazmat imprint, or the
-          environmental threat of biomedical waste – Let GIC Medial Disposal shield your
-          facility from any non-compliance penalties.
-        </h4>
+    <p>
+      <strong>Best Price Guaranteed!</strong><br>
+      <strong>GIC will BEAT any official quote by a minimum Of 10%.</strong>
+    </p>
 
-        <p>
-          It's no secret, extreme caution is required when handling medical waste.  The
-          provincial and federal governments have stringent laws and regulations in place
-          regarding the management of medical waste.  It may be confusing to sort through
-          all the information available and stay on top of regulations.  Improper waste
-          management that is not in compliant with government regulations, can lead to
-          major complications.
-        </p>
+    <p>We combine superior customer service with a wide range of fully integrated solutions to provide you with the best value in the industry. </p>
+    <p>GIC Medical Disposal will tailor a customized disposal solution that will fit your budget and work seamlessly within your facility. Contact us today to get started! </p>
 
-        <p>
-          Count on GICMD to help you and your staff learn the correct methodologies to
-          take care of your facility's medical waste. We will work together with you to
-          develop complete safety awareness among your medical staff and employees.
-        </p>
-
-        <p>
-          We advocate and exercise rigid and compliant procedures, which altogether reduce
-          the risk of accident and/or injury while giving you the freedom to effortlessly
-          focus your attention on managing other areas of your business.
-        </p>
-
-        <h3>Premium Level Customer Service</h3>
-
-        <p>
-          A compliant, efficient and unparalleled customer-focused solution will be
-          designed to meet YOUR specific requirements.
-        </p>
-
-        <ul>
-          <li>Always available during business hours.</li>
-          <li>You will ALWAYS be able to speak to someone that can address your concerns.</li>
-          <li>
-            Flexible – We will customize our service for YOUR specific needs – If you need
-            us to pick up more often, or less often… We will adjust our service to fits your
-            facilities' requirements and operating hours.
-          </li>
-          <li>Industry leading products that meet the highest standards</li>
-          <li>No long-term contracts</li>
-          <li>No monthly/hidden fees</li>
-          <li>Courteous, fully insured staff</li>
-        </ul>
-
-        <h3>Best Price Guaranteed!</h3>
-
-        <h4>GIC will BEAT any official quote by a minimum Of 10%.</h4>
-
-        <p>We combine superior customer service with a wide range of fully integrated
-          solutions to provide you with the best value in the industry.
-        </p>
-
-        <p>GIC Medical Disposal will tailor a customized disposal solution that will fit
-          your budget and work seamlessly within your facility. Contact us today to get
-          started!
-        </p>
-
-        <div class="ac">
-          <a href="http://app.funneltrax.com/traffic/traffic-count/6037" class="bigGreenButton bigGreenButton--inline titleCase spaceAround">CLICK HERE TO GET A QUOTE TODAY!</a>
-        </div>
-
-        <h4>Services Offered</h4>
-        <div>
-          <img src="img/side-1.png" width="180" height="334" alt="Services Offered" class="stock">
-          <ul>
-            <li>Biomedical Waste Disposal</li>
-            <li>Sharps Waste Disposal</li>
-            <li>Chemical Waste Disposal</li>
-            <li>Pharmaceuticals Waste Disposal</li>
-            <li>Sustainability Services</li>
-            <li>Compliance Training</li>
-            <li>Hazardous/Industrial Waste Disposal</li>
-            <li>Vacuum Truck Service</li>
-            <li>Hazardous Material Transportation Service</li>
-            <li>Environmental Remediation / Spill Clean Up</li>
-          </ul>
-
-          <p>
-            We also offer a wide assortment of eco-friendly waste disposal products/. Find
-            out which one is the perfect fit for your business and facility.
-          </p>
-
-          <ul>
-            <li>Sharps waste container, disposable or reusable</li>
-            <li>Biomedical bins and containers</li>
-            <li>Pharmaceutical pails, totes, and drums</li>
-            <li>Chemical pails, totes, and drums</li>
-          <ul>
-        </div>
     </div>
-    <br>
     <div class="clear"></div>
   </div>
 
-<?php include 'components/right.php'; ?>
-<?php include 'components/footer.php'; ?>
+  <?php include 'components/right.php'; ?>
+  <?php include 'components/footer.php'; ?>
